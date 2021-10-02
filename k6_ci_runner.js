139d6ae4131052304319dcf510748fc6ff565d6e
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-10-02 18:32:15 +0200

    added runner

--- k6_ci_runner.js
+++ k6_ci_runner.js
@@ -1,16 +1,26 @@
 import http from "k6/http";
 import { check, group, sleep } from "k6";
-import tagForTest from module1;
+import tagForTest from "./module1";
 
 const ciOptions = {
-  exec: 'ci',
+  scenarios: {
+    ci: {
+      exec: "ci",
+    },
+  },
 };
 const regressionOptions = {
-  exec: "regression",
+  scenarios: {
+    regression: {
+      exec: "regression",
+    },
+  },
 };
 
 export const options = eval(__ENV.mode || "regressionOptions");
 export function ci() {
-  eval(`${__ENV.exec}()`)
+  eval(`${__ENV.exec}()`);
 }
-export function regression() {}
+export function regression() {
+  //all really - iterate
+}
