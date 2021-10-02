303acc6df52e3e669f689221ed586721284da31b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-10-02 18:37:04 +0200

    added runner

--- k6_ci_runner.js
+++ k6_ci_runner.js
@@ -1,10 +1,11 @@
 import http from "k6/http";
 import { check, group, sleep } from "k6";
-import tagForTest from "./module1";
+import tagForTest from "./module1.js";
 
 const ciOptions = {
   scenarios: {
     ci: {
+      executor: 'shared-iterations',
       exec: "ci",
     },
   },
@@ -12,6 +13,7 @@ const ciOptions = {
 const regressionOptions = {
   scenarios: {
     regression: {
+      executor: 'shared-iterations',
       exec: "regression",
     },
   },
